6501068b9e8994f4031fca7fec66a571e5c4983b
wf9a5m75 <wf9a5m75@gmail.com>
2018-10-15 18:48:42 -0700

    creating test case

--- tests/tests.js
+++ tests/tests.js
@@ -1,11 +1,24 @@
 exports.defineAutoTests = function () {
 
+  var config = {
+    apiKey: "AIzaSyA898h_Ez99cQUfmhkAVKT2BmivNhHm_3Y",
+    databaseURL: "https://geofire-test-34492.firebaseio.com"
+  };
+  var app, database;
   describe('FirebaseCore (plugin.firebase.app)', function () {
 
     it('plugin.firebase.app should exist', function () {
         expect(plugin.firebase.app).toBeDefined();
     });
-
+    it('app = plugin.firebase.initializeApp() should be fine', function () {
+        expect(plugin.firebase.initializeApp).toBeDefined();
+        app = plugin.firebase.initializeApp(config);
+        expect(app !== null).toBe(true);
+    });
+    it('database = app.database() should be fine', function () {
+        database = app.database();
+        expect(database !== null).toBe(true);
+    });
   });
 
 };
